3cb8e7ca1a6bd43489f7f5d4e3093a809c58476d
Nicholas Fett <nicholasfett@Nicholass-MacBook-Pro.local>
2023-03-24 13:23:17 -0400

    mumbai oracle deposit script

--- scripts/tellorPush.js
+++ scripts/tellorPush.js
@@ -12,23 +12,14 @@ const { abi, bytecode } = require("usingtellor/artifacts/contracts/TellorPlaygro
 tellorAddress = "0xD9157453E2668B2fc45b7A803D3FEF3642430cC0"
 goerliAddress =  '0x6E2eCf3adec22D80AC3f96479C734e6eB4DFD090'
 chiadoAddress = "0x20301cC7f8d4c734Fd3EAa6038ee3693e0fe8443"
-
+mumbaiAddress = "0x2157EE35E7ecc7B66Ad61B82A79d522a44B1aa84"
+e2p =  "0x2A51B6F68c38625fa0404b2a7ebA8B773e1220A6"
+p2e =  "0xFfED80cF5c45e7463AFd9e0fc664B5C6583B4363"
 startTime = 0;
 
-async function runChecks() {
+async function tellorPush() {
     let _networkName = hre.network.name
     let tellor
-
-    if(_networkName == "goerli"){
-        base = "https://goerli.etherscan.io/address/"
-    }
-    else if(_networkName == "chiado"){
-        base = "https://blockscout.chiadochain.net/address/"
-    }
-    else{
-        console.log("No network name ", _networkName, " found")
-        process.exit(1)
-    }
 
     await run("compile")
     console.log("running oracle deposit on :  ", _networkName)
@@ -37,6 +28,9 @@ async function runChecks() {
     tellor = await hre.ethers.getContractAt(abi, tellorAddress,hre.provider);
     goerliCharon = await hre.ethers.getContractAt("charonAMM/contracts/Charon.sol:Charon", goerliAddress)
     chiadoCharon = await hre.ethers.getContractAt("charonAMM/contracts/Charon.sol:Charon", chiadoAddress)
+    mumbaiCharon = await hre.ethers.getContractAt("charonAMM/contracts/Charon.sol:Charon", mumbaiAddress)
+    p2e = await hre.ethers.getContractAt("charonAMM/contracts/bridges/POLtoETHBridge.sol:POLtoETHBridge", p2e)
+    e2p = await hre.ethers.getContractAt("charonAMM/contracts/bridges/ETHtoPOLBridge.sol:ETHtoPOLBridge",e2p)
 
     if(_networkName == "chiado"){
         let gnoNode = process.env.NODE_URL_GOERLI;
@@ -87,9 +81,79 @@ async function runChecks() {
 
     }
     else if(_networkName == "goerli"){
-
+        let chiNode = process.env.NODE_URL_CHIADO;
+        const provider = new ethers.providers.JsonRpcProvider(chiNode);
+        const wallet = new ethers.Wallet(process.env.PK, provider);
+        const signer = wallet.provider.getSigner(wallet.address)
+        
+        //goerliCharon = new ethers.Contract(goerliCharon,"charonAMM/contracts/Charon.sol:Charon", provider)
+        chiadoCharon = await hre.ethers.getContractAt("charonAMM/contracts/Charon.sol:Charon", chiadoAddress, signer)
+        toSubmit = []
+        inputIds = []
+        let filter = chiadoCharon.filters.DepositToOtherChain()
+        let events = await chiadoCharon.queryFilter(filter, startTime , "latest")
+        filter = goerliCharon.filters.OracleDeposit()
+        let  oracleEvents = await goerliCharon.queryFilter(filter, startTime , "latest")
+        for(i = 0; i< oracleEvents.length; i++){
+            thisId = oracleEvents[i]._inputData;
+            inputIds.push(thisId);
+        }
+        for(i = 0; i< events.length; i++){
+            if(inputIds.indexOf(events[i].args._depositId) == -1){
+                    toSubmit.push(events[i].args._depositId)
+            }
+            console.log("need push for Id's: ", toSubmit)
+        }
+        let _query,_value, _tx, _ts
+        for(i=0;i<toSubmit.length;i++){
+            _query = await getTellorData(tellor,chiadoAddress,10200,toSubmit[i]);
+            _value = await chiadoCharon.getOracleSubmission(toSubmit[i]);
+            if(_query.nonce > 0){
+                //check if 12 hours old
+                _ts = await tellor.getTimestampbyQueryIdandIndex(_query.queryId,0)
+                //if yes do oracle deposit
+                if(Date.now()/1000 - _ts > 86400/2){
+                    _encoded = await ethers.utils.AbiCoder.prototype.encode(['uint256'],[toSubmit[i]]);
+                    await goerliCharon.oracleDeposit([0],_encoded);
+                    console.log("oracleDeposit for id :", toSubmit[i])
+                }
+                else{
+                    console.log("need more time for Id: ",toSubmit[i])
+                }
+            }
+            else{
+                _tx = await tellor.submitValue(_query.queryId, _value,_query.nonce, _query.queryData);
+                console.log("submitting value for id :", toSubmit[i])
+            }
+        }
     }
-
+    else if(_networkName == "mumbai"){
+        //to do, loop through all stateId's and check if they've been submitted, start at oldest
+        let stateIds = await p2e.getStateIds()
+        console.log("stateIds", stateIds);
+        let toSubmit = []
+        let inputIds = []
+        filter = mumbaiCharon.filters.OracleDeposit()
+        let  oracleEvents = await goerliCharon.queryFilter(filter, startTime , "latest")
+        for(i = 0; i< oracleEvents.length; i++){
+            thisId = oracleEvents[i]._inputData;
+            inputIds.push(thisId);
+        }
+        console.log("oracle pushes happened ", inputIds)
+        for(i = 0; i < stateIds.length; i++){
+            //see if stateID pushed already
+            if(inputIds.indexOf(stateIds[i]) == -1){
+                console.log("need to push ", stateIds[i])
+                toSubmit.push(stateIds[i])
+            }
+        }
+        for(i = 0; i < toSubmit.length; i++){
+            let _id = await ethers.utils.AbiCoder.prototype.encode(['uint256'],[toSubmit[i]]);
+            await mumbaiCharon.oracleDeposit([0],_id)
+            console.log("oracle deposit synced on mumbai!", toSubmit[i])
+        }
+        console.log("oracle deposit synced on mumbai done")
+    }
 }
 
 async function getTellorData(tInstance,cAddress,chain,depositID){
@@ -109,7 +173,7 @@ async function getTellorData(tInstance,cAddress,chain,depositID){
         return({queryData: queryData,queryId: queryId,nonce: nonce})
   }
 
-runChecks()
+tellorPush()
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
